96e9d4479dbccb53dbf534fd34cf196efd343d7f
Samm <44311301+KrAsH-CoD3@users.noreply.github.com>
2026-03-13 13:59:34 +0100

    feat: Add Jinja2 templates and static files to serve html file

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import PlainTextResponse
-app = FastAPI()
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
-@app.get("/", response_class=PlainTextResponse)
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+
+@app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return "<h1>It worked.</h1>"
+    return templates.TemplateResponse("index.html", {"request": request})
+
