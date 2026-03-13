31a0007e4d86ec1d03c4a1f1dbed53139a6972c2
Samm <44311301+KrAsH-CoD3@users.noreply.github.com>
2026-03-13 14:01:36 +0100

    feat: Add MongoDB persistence and /save endpoint

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import PlainTextResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+from datetime import datetime, timezone
+import os
+
+load_dotenv()
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+client = AsyncIOMotorClient(os.getenv("MONGODB_URI"))
+collection = client[os.getenv("DB_NAME", "fingrasp")]["fingerprints"]
 
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+
+@app.post("/save")
+async def save(request: Request):
+    data = await request.json()
+    data["ip"]       = request.client.host
+    data["saved_at"] = datetime.now(timezone.utc).isoformat()
+    result = await collection.insert_one(data)
+    return {"id": str(result.inserted_id)}
